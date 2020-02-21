735a9ed0c7ebbcdd5dee1cc15629fefb8a9dd881
muhammadfarras <43328916+muhammadfarras@users.noreply.github.com>
2020-02-21 14:44:50 +0700

    Add

--- exception/exception-awal.php
+++ exception/exception-awal.php
@@ -47,7 +47,11 @@
 				
 				<fieldset id="Exception">
 					<legend>Exception</legend>
-                    <p></p>
+                    <p>Exception dasar terdiri dari try dan catch. Mekanisme kerjanya, kita harus membuat function atau method kemungkinan kondisi
+                    error yang akan terjadi dengan keywoard <em class="target">throw</em> & membuat objek <em class="target">Exception</em>.</p>
+                    <p>Masukan sebuah function kedalam codeblock try, <em class="strong">menguji apakah kode berikut mengashilkan error atau tidal</em></p>
+                    <p>JIka iya, maka PHP akan melemparnya dan akan ditangkap oleh keywoard <em class="target">catch</em> untuk ditampilkan informasi
+                    mengenai error yang terjadi</p>
                     <hr>
                     <div>
                         <?php
@@ -56,11 +60,17 @@
                                 if ($a === 0 ){
                                     throw new Exception ("Tidak dapat dibagi dengan no",201);
                                 }
+                                else {
+                                    return "<h3>Angka yang dipilih adalah </h3>";
+                                }
                             }
                             
                             try {
+                                echo foo (24);
                                 echo foo (0);
+                                echo foo (20);
                             }
+                            
                             catch (Exception $e) {
                                 echo "Pesan error : ".$e->getMessage()    ."<br>";
                                 echo "Kode error : ".$e->getCode()    ."<br>";
@@ -70,6 +80,7 @@
                                 
                                 echo "<pre>";
                                 print_r ($e->getTrace());
+                                print_r (get_class_methods ('Exception'));
                                 echo "</pre>";
                             }
                             
@@ -77,8 +88,58 @@
                         <br>
                         <embed src="../script/function-is-obj.pdf" width=600px height="250">
                     </div>
+                    
+                    <p>Dapat dilihat pada baris ke 65 dan 67, saya memanggil function foo () tiga kali, namun mengapa yang keluar
+                    hanya 2 nilai, nilai dari function 1 dan function 2 (error).<em class="strong">Hal ini terjadi karena
+                    pada code block try ketika ada function yang dilempar error, langsung menuju code block catch tanpa balik lagi
+                    ke code block try.</em></p>
+                <p>
+                    Solusi untuk penggunaan function lebih dari satu dan memiliki masing2 exception,maka kita bisa membuat exception u/ setiap
+                    function yang dipanggil.
+                </p>
                 </fieldset>
                 
+                <fieldset id="Exception Catch Funtion Terpisah">
+					<legend>Exception Catch Funtion Terpisah</legend>
+                    <p>Untuk menangkap error kita bisa dengan membuat function terpisah sesuai dengan kebutuhan</p>
+                    <hr>
+                    <div>
+                        <?php
+                            
+                            function  isString ($text){
+                                $result = is_string ($text);
+                                $len = strlen ($text);
+                                
+                                if (!$result){
+                                    throw new Exception ("<h3>Input harus berupa string</h3>");
+                                }
+                                elseif ($len >30){
+                                    throw new Exception ("<h3>Tidak boleh lebih dari 30 karakter</h3>");
+                                }
+                                return "<h3>$text</h3>";
+                            }
+                            
+                            function showError ($e){
+                                return $e->getMessage();
+                            }
+                            
+                            try {
+                                echo isString ("Hai farras apa kabar"); // No error
+                                echo isString (271295); // Error
+                                echo isString ("Hai farras apa kabarHai farras apa kabarHai farras apa kabarHai
+                                               farras apa kabarHai farras apa kabar"); // Error
+                            }
+                            catch ( Exception $e ){
+                                echo showError ($e);
+                            }
+                            
+                        ?>
+                        <br>
+                        <embed src="../script/exception-lebih-dari-satu.pdf" width=600px height="250">
+                    </div>
+                </fieldset>
+                
+                    
         </div>
     </div>
     
